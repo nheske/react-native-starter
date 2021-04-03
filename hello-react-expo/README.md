46d557a6731c57ae7c873aa91b1dd2e4695bec2b
nheske <normheske@gmail.com>
2021-04-03 16:24:38 -0700

    react native cli

--- hello-react-expo/README.md
+++ hello-react-expo/README.md
@@ -1,4 +1,4 @@
 based on [React Native Tutorial #2 (2021) - Creating first app with Expo}(https://www.youtube.com/watch?v=p1Csi_0rh5I&list=PLqGdu3H2yIjax3haGsMs2MST6zD4Pux9Y&index=10)
 
 * install expo: yarn global add expo-cli
-* create project: expo init my-project
+* create project: expo init hello-react-expo
---
+++ hello_react_native_cli/App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () =>  {
+  return (
+    <View style={styles.body}>
+      <Text>Norm</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#ffffff',
+  },
+});
+
+export default App;
