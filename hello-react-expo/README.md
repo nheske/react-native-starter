1c3432dddfcc9149fa6e4eee6f15b80b4fe7ef31
nheske <normheske@gmail.com>
2021-04-03 17:14:26 -0700

    more state

--- hello-react-expo/README.md
+++ hello-react-expo/README.md
@@ -2,3 +2,6 @@ based on [React Native Tutorial #2 (2021) - Creating first app with Expo}(https:
 
 * install expo: yarn global add expo-cli
 * create project: expo init hello-react-expo
+
+[React Native Tutorial Playlist:](https://www.youtube.com/playlist?list=PL8kfZyp--gEXs4YsSLtB3KqDtdOFHMjWZ)
+[Source Code in Github:] (https://github.com/mahdi-sharifimehr/RN-Tutorial-Main)
--- hello_react_native_cli/App.js
+++ hello_react_native_cli/App.js
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Button,
   Linking,
@@ -16,9 +9,35 @@ import {
 } from 'react-native';
 
 const App = () =>  {
+  const [name, setName] = useState('Treb')
+  const [session, setSession] = useState({ number: 6, title: 'state'})
+  const [current, setCurrent] = useState(true)
+  const [clicks, setClicks] = useState(0)
+
+  const [sum, setSum] = useState(0)
+
+  const onClickHandler = ()=>{
+    setName('programming')
+    setSession({ number: 7, title: 'style'})
+    setCurrent(false)
+  }
+
+  const addHandler = ()=>{
+    setClicks(clicks+1)
+    setSum(sum+5)
+  }
+
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Norm</Text>
+      <Text style={styles.text}>{sum}</Text>
+      <Button title='add' onPress={addHandler}></Button>
+      <Text style={styles.text}>{clicks}</Text>
+
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>This is session number {session.number} and about {session.title}</Text>
+      <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
+      <Button title='name' onPress={onClickHandler}></Button>
       <Button title='Youtube' onPress={()=>{Linking.openURL('https://youtube.com/programmingwithmash')}}></Button>
 
     </View>
