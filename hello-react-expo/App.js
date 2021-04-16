fe34859bab58b03fdf469e8c31469d1efbb0cb23
nheske <you@example.com>
2021-04-15 19:18:22 -0700

    flatlist

--- hello-react-expo/App.js
+++ hello-react-expo/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native Expo App12</Text>
+      <Text>Hello Meetup</Text>
       <StatusBar style="auto" />
+      <Text style={{color: 'blue'}}
+      onPress={() => Linking.openURL('https://www.meetup.com/sandiegojs')}>Some Link</Text>
     </View>
   );
 }
--- hello_react_native_cli/App.js
+++ hello_react_native_cli/App.js
@@ -2,6 +2,7 @@
 import React, {useState} from 'react';
 import {
   Button,
+  FlatList,
   Linking,
   StyleSheet,
   Text,
@@ -26,35 +27,58 @@ const App = () =>  {
     setClicks(clicks+1)
     setSum(sum+5)
   }
-
+  const [Items, setItems] = useState([
+    { key: '1', name: 'Item 1' },
+    { key: '2',name: 'Item 2' },
+    { key: '3',name: 'Item 3' },
+    { key: '4',name: 'Item 4' },
+    { key: '5',name: 'Item 5' },
+    { key: '6',name: 'Item 6' },
+    { key: '7',name: 'Item 7' },
+    { key: '8',name: 'Item 8' },
+    { key: '9', name: 'Item 9' },
+    { key: '101',name: 'Item 27' },
+    { key: '11',name: 'Item 78' },
+  ]);
 
   return (
-    <View style={styles.body}>
-      <Text style={styles.text}>{sum}</Text>
-      <Button title='add' onPress={addHandler}></Button>
-      <Text style={styles.text}>{clicks}</Text>
+    <FlatList 
+      data={Items} 
+      renderItem={({item}) => (
+        <View style={styles.item}>
+         <Text>{item.name}</Text>
+        </View>
+    )}
+  />
+    // <View style={styles.body}>
+    //   <Text style={styles.text}>{sum}</Text>
+    //   <Button title='add' onPress={addHandler}></Button>
+    //   <Text style={styles.text}>{clicks}</Text>
 
-      <Text style={styles.text}>{name}</Text>
-      <Text style={styles.text}>This is session number {session.number} and about {session.title}</Text>
-      <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
-      <Button title='name' onPress={onClickHandler}></Button>
-      <Button title='Youtube' onPress={()=>{Linking.openURL('https://youtube.com/programmingwithmash')}}></Button>
-
-    </View>
+    //   <Text style={styles.text}>{name}</Text>
+    //   <Text style={styles.text}>This is session number {session.number} and about {session.title}</Text>
+    //   <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
+    //   <Button title='name' onPress={onClickHandler}></Button>
+    //   <Button title='Youtube' onPress={()=>{Linking.openURL('https://youtube.com/programmingwithmash')}}></Button>
+    // </View>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#aaaaff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
   },
-
+  item: {
+    margin: 10,
+    backgroundColor: '#4ae1f1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   text: {
-    color: '#ffffff',
-    fontSize: 20,
+    color: '#000000',
+    fontSize: 45,
     fontStyle: 'italic',
     margin: 10,
   },
