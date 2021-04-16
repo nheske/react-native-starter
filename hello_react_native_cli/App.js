38725bf5c19214db4db78e805ae6cf69c770f7d0
nheske <you@example.com>
2021-04-15 20:18:20 -0700

    need to fix url

--- hello_react_native_cli/App.js
+++ hello_react_native_cli/App.js
@@ -8,21 +8,29 @@ import {
   RefreshControl,
   FlatList,
   SectionList,
+  Linking,
 } from 'react-native';
+
 
 const App = () => {
   const [name, SetName] = useState('');
+  let url = 'https://www.google.com/search';
+  url +='?q=myvalue';
+  const [name, SetName] = useState('');
+ // const [url, setState(prevState => ({ ...prevState, [name]: value }));] = useState('https://www.google.com/search?q=');
   return (
     <View style={styles.body}>
       <Text>Please enter name</Text>
       <TextInput
         style={styles.input}
         placeholder='e.g. John'
-        onChangeText={(value) => SetName(value)}
+        onChangeText={(value) => SetName(url+value)}
       />
       <Text style={styles.text}>
         Your name is: {name}
       </Text>
+      <Text style={{color: 'blue'}}
+      onPress={() => Linking.openURL(url)}>search for {name}</Text>
     </View>
   );
 };
