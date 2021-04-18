bca9676373b96b108a0c1c4e1770db17a951c29b
nheske <you@example.com>
2021-04-18 14:05:54 -0700

    add native paper

---
+++ native-paper-nav/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthStackNavigator from './src/navigation/AuthStackNavigator'
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <AuthStackNavigator/>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
